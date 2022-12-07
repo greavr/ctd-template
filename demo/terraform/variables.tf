42c6130b84861a3bc2daf8a7aa8e01c4a5f697f1
Rick Greaves <rgreaves@google.com>
2022-12-07 11:33:10 -0800

    Simplifying the process

--- demo/terraform/variables.tf
+++ demo/terraform/variables.tf
@@ -5,28 +5,35 @@ variable "project_id" {
   type        = string
   description = "project id required"
 }
-# variable "project_name" {
-#  type        = string
-#  description = "project name in which demo deploy"
-# }
-# variable "project_number" {
-#  type        = string
-#  description = "project number in which demo deploy"
-# }
-# variable "gcp_account_name" {
-#  description = "user performing the demo"
-# }
-# variable "deployment_service_account_name" {
-#  description = "Cloudbuild_Service_account having permission to deploy terraform resources"
-# }
-# variable "org_id" {
-#  description = "Organization ID in which project created"
-# }
-# variable "data_location" {
-#  type        = string
-#  description = "Location of source data file in central bucket"
-# }
-# variable "secret_stored_project" {
-#   type        = string
-#   description = "Project where secret is accessing from"
-# }
+variable "project_name" {
+  type        = string
+  description = "project name in which demo deploy"
+  default = ""
+}
+variable "project_number" {
+  type        = string
+  description = "project number in which demo deploy"
+  default = ""
+}
+variable "gcp_account_name" {
+  description = "user performing the demo"
+  default = ""
+}
+variable "deployment_service_account_name" {
+  description = "Cloudbuild_Service_account having permission to deploy terraform resources"
+  default = ""
+}
+variable "org_id" {
+  description = "Organization ID in which project created"
+  default = ""
+}
+variable "data_location" {
+  type        = string
+  description = "Location of source data file in central bucket" 
+  default = ""
+}
+variable "secret_stored_project" {
+    type        = string
+    description = "Project where secret is accessing from"
+    default = ""
+}
--- tools/ReadMe.md
+++ tools/ReadMe.md
@@ -1,4 +1,5 @@
 ## Install gcloud
+### Linux
 Install Guide [Found Here](https://cloud.google.com/sdk/docs/install)
 ```
 sudo apt-get install -y apt-transport-https ca-certificates gnupg
@@ -11,29 +12,38 @@ Then login to the GCLOUD toolkit with:
 gcloud init
 ```
 
+### Mac OSX
+Install Guide [Found Here](https://cloud.google.com/sdk/docs/install-sdk#mac)
+
 ## Install TF
+### Linux
 Install Guide [Found Here](https://learn.hashicorp.com/tutorials/terraform/install-cli)
-```
+```bash
 sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update && sudo apt-get install terraform
 terraform -install-autocomplete
 ```
+### Mac OSX
+Install Guide [Found Here](https://developer.hashicorp.com/terraform/tutorials/aws-get-started/install-cli)
 
 ## Install Kubectl
-```
+### Linux
+```bash
 sudo apt-get install kubectl
 ```
+### Mac OSX
+Install Guide [Found Here](https://kubernetes.io/docs/tasks/tools/install-kubectl-macos/)
 
 ## Install SKAFFOLD
 ### Linux
-```
+```bash
 curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 && \
 sudo install skaffold /usr/local/bin/
 ```
 ### Mac OSX
-```
+```bash
 curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-darwin-amd64 && \
 sudo install skaffold /usr/local/bin/
 ```
