318ed7b24478d48110880af26fe62a1f5c151d72
Rick Greaves <greavr@gmail.com>
2023-01-17 15:21:04 -0800

    Update versions.tf

--- demo/terraform/versions.tf
+++ demo/terraform/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 3.48.0"
+      version = "~> 4.10.0"
     }
     google-beta = {
       source = "hashicorp/google-beta"
